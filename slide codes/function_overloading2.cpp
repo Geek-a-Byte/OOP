21e8a03eff8d52a847b4d028a5083c07403c42b7
Geek-a-Byte <ncoder76@gmail.com>
2021-02-12 23:32:24 +0600

    func overloading

--- slide codes/function_overloading2.cpp
+++ slide codes/function_overloading2.cpp
@@ -1,40 +1,43 @@
 //! error in code
 #include <bits/stdc++.h>
 using namespace std;
-int sum(int a, int b)
+
+int abs(int n)
 {
-    int result = a + b;
-    return result;
+    return (n < 0) ? -n : n; //-(-7)=7
 }
-float sum(float a, float b)
+double abs(double n)
 {
-    float result = a + b;
-    return result;
+    return (n < 0) ? -n : n;
 }
-// int abs(int n)
-// {
-//     return (n < 0) ? -n : n;
-// }
-// long abs(long n)
-// {
-//     return (n < 0) ? -n : n;
-// }
 // float abs(float n)
 // {
 //     return (n < 0) ? -n : n;
 // }
 // template <class T>T function_name(T n){ }
 // template <typename T>T function_name(T n){ }
-template <class T>
-T function_name(T n) {}
+//template <class T>
+//T function_name(T n) {}
+int sum(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+double sum(double a, double b)
+{
+    double result = a + b;
+    return result;
+}
 main()
 {
-    // int i = -3;
-    // cout << abs(i);
-    // long l = -7;
-    // cout << abs(l);
+    int i = -3;
+    cout << abs(i) << endl;
+    long l = -7.00;
+    cout << abs(l) << endl;
+
     // float f = -2.0;
     // cout << abs(f);
-    //cout << sum(3.40000, 5.6000);error
+    cout << sum(3.400000, 5.60000) << endl;
+
     cout << sum(3, 7);
 }
