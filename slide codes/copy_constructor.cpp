c24616550d73a0b4722cdd755303ffb056cf48b6
Geek-a-Byte <ncoder76@gmail.com>
2021-02-14 19:35:18 +0600

    test

--- slide codes/copy_constructor.cpp
+++ slide codes/copy_constructor.cpp
@@ -61,14 +61,15 @@ void func2(Student *st)
 int main()
 {
     Student st(2, "nazia"), st2;       //explicitly
-    Student st1 = Student(3, "nawme"); //implicitly
+    Student st1 = Student(3, "nawme"); //implicitly ->Student st1(3,"nawme")
+    Student st3 = st;                  //new object=existing obj ->copy constructor called
     st = st1;                          //just an assignment operator; st is already instantiated, so when it is assigned a new value, copy constructor wont be called
     // copy constructor is called when a new object is called from an existing object as a copy of the existing object
     cout << st.getName() << endl;
     cout << "hello\n";
-    func(st1);   //destructor called
-    func1(st1);  //no dest called
-    func2(&st1); //no dest called
+    func(st1);   //cc called destructor called
+    func1(st1);  //no cc called no dest called
+    func2(&st1); //no cc/dest called
     //cout << st.getID();
     //cout << endl;
     // cout << st1.getID();
